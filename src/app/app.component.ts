55ec2d80025d7e6b0d06939d942efee11411af63
paulsoniat <paulmsoniat@gmail.com>
2020-01-23 17:35:53 -0600

    [Themes] Adds Dark mode theme, light mode - Css vaiables are close to working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { apiService } from './api.service';
 import { Tweet } from './tweets';
+import { ThemeService } from './theme/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,22 @@ import { Tweet } from './tweets';
 })
 
 export class AppComponent implements OnInit {
+  constructor(private apiService: apiService, private themeService: ThemeService){ }
   title = 'demoJobTest';
   public dataLoaded: boolean;
   public tweetData: Array<Tweet>;
   public tweetDataFromServer: Array<string>;
-  constructor(private apiService: apiService){ }
   
+  toggle() {
+    const active = this.themeService.getActiveTheme() ;
+    console.log(active);
+    if (active.name === 'light') {
+      this.themeService.setTheme('dark');
+    } else {
+      this.themeService.setTheme('light');
+    }
+  }
+
   ngOnInit(){
     this.apiService.getContacts().subscribe((res)=>{
       this.tweetData = res;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { HttpClientModule } from '@angular/common/http';
+import { ThemeModule } from './theme/theme.module';
+import { lightTheme } from './theme/light-theme';
+import { darkTheme } from './theme/dark-theme';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,11 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ThemeModule.forRoot({
+      themes: [lightTheme, darkTheme],
+      active: 'light'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
